80e1d417a2781f02d8148f61a5e06436ba335043
Ezeanyim Henry <ezeanyimhenry@gmail.com>
2022-08-28 11:24:22 +0100

    login page blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,77 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" href="./assets/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/main.css" />
+    <link rel="stylesheet" href="./assets/signup.css" />
+</head>
 
-                <div class="card-body">
+<body>
+    <div class="container-fluid signup">
+        <div class="row">
+            <div class="col-md-7 pics">
+                <div>
+                    <img src="./assets/imgs/logo/suplogo.png" alt="" />
+                    <h2>track jobs easy way!</h2>
+                </div>
+                <img src="./assets/imgs/signin.png" alt="" />
+            </div>
+            <div class="col-md-5 form">
+                <img src="./assets/imgs/logo/mobilelogo.png" class="mobilelogo" alt="" />
+                <div>
+                    <h3>Sign in</h3>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <div class="mb-3">
+                            <label for="inputEmail" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="inputEmail"
+                                placeholder="input your email in here" />
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mb-2">
+                            <label for="inputPassword" class="form-label">Email</label>
+                            <input type="password" class="form-control" id="inputPassword"
+                                placeholder="input your password in here" />
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <div>
+                            @if (Route::has('password.request'))
+                                <a id="forgotpsd" class="mb-4" href="{{ route('password.request') }}">forgot
+                                    password?</a>
+                            @endif
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="btns">
+                            <button type="submit" class="btn col-12 btn-primary mb-2 mt-2">
+                                Sign in
+                            </button>
+                            <span>or</span>
+                            <button type="button" class="btn col-12 btn-primary mb-2 mt-2">
+                                Sign in with google
+                            </button>
                         </div>
+                        <p class="mt-2">
+                            Doesn't have an account? <a href="#">Sign up</a>
+                        </p>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <script src="./assets/bootstrap/bootstrap.min.js"></script>
+</body>
+
+</html>
